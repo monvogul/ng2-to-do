8654223ced925785f7b2ee431aeec465b876faf3
monvogul <toreachmansi@gmail.com>
2018-02-07 23:33:25 +1100

    fixed tslint issues

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {CanActivate, Router} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-import {AuthService} from "./auth.service";
+import {AuthService} from './auth.service';
 import 'rxjs/add/operator/do' ;
 import 'rxjs/add/operator/map' ;
 import * as firebase from 'firebase';
@@ -12,7 +12,7 @@ export class AuthGuard implements CanActivate {
 
   user$: Observable<firebase.User> ;
 
-  constructor(private auth: AuthService, private router: Router){
+  constructor(private auth: AuthService, private router: Router) {
    this.user$ = auth.afAuth.authState ;
   }
 
